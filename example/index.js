7fdbb9719a05aab5b0a5d15f587e39a1276f2985
ryunosuke <ryunosuke.arima@gmail.com>
2020-10-02 20:17:09 +0900

    example 更新

--- example/index.js
+++ example/index.js
@@ -9,18 +9,5 @@ const config = {
 };
 
 const assetter = require('../index');
-assetter.transpiler.regsiter('.stylus', {
-    ext: '.css',
-    mappingURL: url => `\n/*# sourceMappingURL=${url} */`.replace(/\\/g, '/'),
-    compile: async function (input, options) {
-        const fs = require('fs');
-        return new Promise(async (resolve, reject) => {
-            resolve({
-                content: (await fs.promises.readFile(input)).toString().toUpperCase(),
-                mapping: {},
-            });
-        });
-    },
-});
 assetter.httpd(config);
 assetter.fsd(config);
