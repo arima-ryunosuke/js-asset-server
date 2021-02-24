3ac81bfd390ba8b8f22738050b5996b4d992f90e
ryunosuke <ryunosuke.arima@gmail.com>
2021-02-24 21:55:25 +0900

    [example] alias サンプルを追加

--- example/index.js
+++ example/index.js
@@ -2,6 +2,14 @@ const config = {
     routes: {
         "/virtual": __dirname + '/../',
         "/": __dirname + '/../example',
+        "/script/ab.js": [
+            __dirname + '/script/a.es',
+            __dirname + '/script/b.es',
+        ],
+        "/style/ab.css": [
+            __dirname + '/style/a.scss',
+            __dirname + '/style/b.scss',
+        ],
     },
     maps: {
         "/map": __dirname + '/../example/map',
